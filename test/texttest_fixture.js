d266dac29e66ce91e75473ec9445b93631b9cf8e
thashimoto <thahsimoto@.me>
2021-09-15 15:17:53 +0900

    first init

---
+++ test/texttest_fixture.js
@@ -0,0 +1,36 @@
+
+const { Shop, Item } = require("../src/gilded_rose");
+
+const resultTest = () => {
+  const items = [
+    new Item("+5 Dexterity Vest", 10, 20),
+    new Item("Aged Brie", 2, 0),
+    new Item("Elixir of the Mongoose", 5, 7),
+    new Item("Sulfuras, Hand of Ragnaros", 0, 80),
+    new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+    new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+    new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+    new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+
+    // This Conjured item does not work properly yet
+    new Item("Conjured Mana Cake", 3, 6),
+  ];
+
+  const days = Number(process.argv[2]) || 2;
+  const gildedRose = new Shop(items);
+  let result = "";
+  console.log("OMGHAI!");
+  for (let day = 0; day < days; day++) {
+    result += `\n-------- day ${day} --------`;
+    console.log(`\n-------- day ${day} --------`);
+    result += "name, sellIn, quality";
+    console.log("name, sellIn, quality");
+    items.forEach(item => {
+      result += `${item.name}, ${item.sellIn}, ${item.quality}`;
+      console.log(`${item.name}, ${item.sellIn}, ${item.quality}`)
+    });
+    gildedRose.updateQuality();
+  }
+  return result;
+}
+resultTest();
